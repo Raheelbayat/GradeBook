688c4b334a798161059429124e5c1dde3e19382b
Raheel Bayat <bayatraheel@gmail.com>
2026-07-01 12:28:59 +0430

    new changes in methods of student class

--- student.py
+++ student.py
@@ -11,20 +11,33 @@ class Student:
     def get_full_name(self):
         return self.full_name
 
-    def set_email(email):
-        self.email = self.full_name
-        return self.email
+    def set_email(self, email):
+        if "@" in email and "." in email:
+            self.email = email
+            print("Email updated successfully.")
+        else:
+            print("Invalid email address.")
 
 
-    def enroll_course(course_code):
-        self.courses.append(course_code)  #would not work! first make the course code dictionary or list then try
+    def enroll_course(self, course_code):
+        if course_code not in self.courses:
+            self.courses.append(course_code)
+            print(f"{course_code} enrolled successfully.")
+        else:
+            print(f"{course_code} is already enrolled.")
 
 
     def display_info(self):
-        print(f"Student ID: {self.student_id}"
+        print(f"\nStudent ID: {self.student_id}"
               f"\nFull name: {self.full_name}"
-              f"\nEmail: {self.email}"
-              f"\nEnrolled Courses: {self.enroll_course}")
+              f"\nEmail: {self.email}")
 
-s1 = Student("S101", "Mohammad Karim", "Karimmoh12@gmail.com", ["", ""])
+        if self.courses:
+            print("Courses:", ", ".join(self.courses))
+        else:
+            print("Courses: No courses enrolled")
+
+s1 = Student("S101", "Mohammad Karim", "Karimmoh12@gmail.com", ["Quran", "Math"])
 s1.display_info()
+s2 = Student("S002", "Ahmad Murtazawi", "Qaramurteaza@yahoo.com", ["German"] )
+s2.display_info()
