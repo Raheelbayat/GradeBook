589549a295abcf0608c7a618efb03cbc7f393bae
Raheel Bayat <bayatraheel@gmail.com>
2026-07-13 10:26:59 +0330

    testing

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from student import Student
 from course import Course
+from assessment import Assessment
 from quiz import Quiz
 from exam import Exam
-from project import Project
+from Project import Project
 from gradebook import Gradebook
 
-gb = Gradebook()
+gradebook = Gradebook()
 
-student1 = Student("S001", "Ahmad Rahimi", "ahmad@example.com", [])
-student2 = Student("S002", "Sara Ali", "sara@example.com", [])
+student1 = Student("S001", "Ahmad Bayat", "ahmad@gmail.com", [])
+student2 = Student("S002", "Munira Bayat", "MN123bayat@yahoo.com", [])
 
-gb.add_student(student1)
-gb.add_student(student2)
+gradebook.add_student(student1)
+gradebook.add_student(student2)
+
+course1 = Course("NM01", "Neuromatch Computational Neuroscience Course")
+gradebook.add_course(course1)
 
